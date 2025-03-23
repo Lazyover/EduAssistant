deb7a65bf5b5bc617890f6588efff748d601449e
YiJIa <1787152643@qq.com>
2025-03-23 19:55:27 +0800

    完善README

--- app/views/auth.py
+++ app/views/auth.py
@@ -1,5 +1,4 @@
 from flask import Blueprint, render_template, redirect, url_for, flash, request, session
-from flask_login import login_user, logout_user, login_required, current_user
 from app.services.user_service import UserService
 
 auth_bp = Blueprint('auth', __name__)
